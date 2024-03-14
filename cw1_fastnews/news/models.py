0fabca95dba941cfc5ae4ef16706d34eb876cc65
Tejaswa Rizyal <rizyal.tejaswa@gmail.com>
2024-03-14 16:34:00 +0000

    database setup done

--- cw1_fastnews/news/models.py
+++ cw1_fastnews/news/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    author_id = models.AutoField(primary_key=True, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+
+class News(models.Model):
+    news_id = models.AutoField(primary_key=True, unique=True)
+    headline = models.CharField(max_length=64)
+    category = models.Choices('pol', 'art', 'tech', 'trivia')
+    region = models.Choices('uk', 'eu', 'w')
+    author = models.ManyToManyField(Author)
+    date = models.DateField()
+    details = models.CharField(max_length=128)
