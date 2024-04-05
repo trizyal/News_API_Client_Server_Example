1c8bfb4a13e4dd664ba88dc76093000bde33671f
Tejaswa Rizyal <rizyal.tejaswa@gmail.com>
2024-04-05 01:40:38 +0100

    updates for django

--- news/models.py
+++ news/models.py
@@ -3,14 +3,24 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(User):
+    # Inherits from User model
+    # To get access to the User model functions
+    # like login, logout, authenticate, etc
+    # All field needed are inherited from User model
+
+    # Display in the admin panel
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+    # Meta class to display the model name in the admin panel
     class Meta:
         verbose_name = "Author"
         verbose_name_plural = "Authors"
 
 class News(models.Model):
+
+    # Choices for the category field
+    # Represented like this because they need to be iterable
     CategoryChoices = (
         ('pol', 'Politics'),
         ('art', 'Art'),
@@ -27,8 +37,10 @@ class News(models.Model):
 
     def __str__(self):
         categories = dict(self.CategoryChoices)
+        # To display the Information in a visually appealing way
         return f"{self.headline} - {categories[self.category]} - {self.author}"
 
+    # Meta class to display the model name in the admin panel
     class Meta:
         verbose_name = "News"
         verbose_name_plural = "News'"
--- news/urls.py
+++ news/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('api/login', views.view_login, name='login'),
+    path('api/login/', views.view_login, name='login/'),
     path('api/logout', views.view_logout, name='logout'),
+    path('api/logout/', views.view_logout, name='logout/'),
     path('api/stories', views.story, name='stories'),
+    path('api/stories/', views.story, name='stories/'),
     path('api/stories/<str:key>', views.delete, name='delete')
 ]
